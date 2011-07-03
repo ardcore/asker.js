2bd80518f98a2cf90891f27aaf5a98f5fd23ee2e
Szymon Pilkowski <szymon.pilkowski@gmail.com>
2011-07-03 04:15:12 +0200

    fixed typo

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ var q = [
 	{ topic: 'age', question: '... and you are?', default: 'developer' },
 	{ topic: 'sex', question: 'What is your sex?', options: ['male', 'female'], mandatory: true },
 	{ topic: 'age', question: 'And your age?', mandatory: true, check: function(answer) { return answer > 10 && answer < 90 } },
-	{ topic: 'hobby', question: 'What\'s your hobby?', callback: function(data) { console.log("Your hobby is", data || " nowhere", ", how cool!")}},
+	{ topic: 'hobby', question: 'What\'s your hobby?', callback: function(data) { console.log("Your hobby is", data || " gaming", ", how cool!")}},
 	{ topic: 'name_correct', question: function(data) { return "is your name really " + data.name + "?" }, options: ['yes', 'no'], mandatory: true }
 ]
 asker.ask(q, function(err, data) {
